05cc55ad0b77eabd5bd3a10a4b7b8ceffa7bc748
James Brendamour <jamesbrendamour7@yahoo.com>
2022-10-11 10:37:19 -0600

    got the Stock_fundamental converted to py file

---
+++ tutorials/1-Introduction/Stock_Fundamental.py
@@ -0,0 +1,35 @@
+
+##%
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+# matplotlib.use('Agg')
+import datetime
+
+from finrl import config
+from finrl import config_tickers
+from finrl.meta.preprocessor.yahoodownloader import YahooDownloader
+from finrl.meta.preprocessor.preprocessors import FeatureEngineer, data_split
+from finrl.meta.env_stock_trading.env_stocktrading import StockTradingEnv
+from finrl.agents.stablebaselines3.models import DRLAgent
+from finrl.plot import backtest_stats, backtest_plot, get_daily_return, get_baseline
+
+from pprint import pprint
+import os
+import sys
+sys.path.append("../FinRL")
+
+import itertools
+
+
+
+if not os.path.exists("./" + config.DATA_SAVE_DIR):
+    os.makedirs("./" + config.DATA_SAVE_DIR)
+if not os.path.exists("./" + config.TRAINED_MODEL_DIR):
+    os.makedirs("./" + config.TRAINED_MODEL_DIR)
+if not os.path.exists("./" + config.TENSORBOARD_LOG_DIR):
+    os.makedirs("./" + config.TENSORBOARD_LOG_DIR)
+if not os.path.exists("./" + config.RESULTS_DIR):
+    os.makedirs("./" + config.RESULTS_DIR)
+
